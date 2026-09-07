b771a1da0b3cf06f917c20479a708ce35710c22f
unknown <mi0b1ue.126@gmail.com>
2026-09-08 06:56:52 +0800

    adapter

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,11 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 import argon2 from 'argon2';
 
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL!
+});
+const prisma = new PrismaClient({ adapter });
 
 async function main() {
     const passwordHash = await argon2.hash(
@@ -17,7 +21,8 @@ async function main() {
             username: "admin",
             nickname: "鹳狸猿",
             password: passwordHash,
-            role: "ADMIN"
+            role: "ADMIN",
+            createdAt: new Date()
         }
     });
 }
